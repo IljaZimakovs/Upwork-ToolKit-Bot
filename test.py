f2648f43d968eafd7ef31385d120eccbc95630ed
DevRex-0201 <dev.rex.0201@gmail.com>
2024-11-07 15:31:18 -0500

    updated script

---
+++ test.py
@@ -0,0 +1,15 @@
+import telegram
+import asyncio
+import os
+# Load environment variables
+# Use os.getenv to retrieve environment variables
+TELEGRAM_TOKEN = "7644435390:AAExmo8L6ODq-ssFKHEjzpHDWMHMC9pOeMc"
+async def send_mail():
+    print('send_mail')
+    bot = telegram.Bot(TELEGRAM_TOKEN)
+    async with bot:
+        print(await bot.get_me())
+        chat_id = (await bot.get_updates())
+        print(chat_id)
+if __name__ == "__main__":
+    asyncio.run(send_mail())
